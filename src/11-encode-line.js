1b45e1ae9112469a26ea20338716b64edf399245
OKitel <kitelolga@gmail.com>
2021-04-08 17:02:21 +0200

    feat: solve encode-line

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,33 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  if (str.length === 0) {
+    return str;
+  }
+  const arr = str.split('');
+  const result = [];
+  let n = 1;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] === arr[i - 1]) {
+      n++;
+    }
+    if (arr[i] !== arr[i - 1]) {
+      if (n !== 1) {
+        result.push(`${n}${arr[i - 1]}`);
+      } else {
+        result.push(`${arr[i - 1]}`);
+      }
+      n = 1;
+    }
+    if (i === arr.length - 1) {
+      if (n !== 1) {
+        result.push(`${n}${arr[i]}`);
+      } else {
+        result.push(`${arr[i]}`);
+      }
+    }
+  }
+  return result.join('');
 }
-
 module.exports = encodeLine;
