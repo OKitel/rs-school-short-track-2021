b5879865fc80bbf71407fb120a380384ce88d6c1
OKitel <kitelolga@gmail.com>
2021-04-08 19:33:55 +0200

    feat: solve dns-stats

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,53 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const obj = {};
+  for (let i = 0; i < domains.length; i++) {
+    const indexLast = domains[i].lastIndexOf('.');
+    const dns1 = domains[i].slice(indexLast + 1);
+    let result = 0;
+    const dnsName = `.${dns1}`;
+    for (let j = 0; j < domains.length; j++) {
+      if (domains[j].endsWith(`.${dns1}`)) {
+        result++;
+      }
+    }
+    obj[dnsName] = result;
+  }
+  for (let i = 0; i < domains.length; i++) {
+    const indexLast = domains[i].lastIndexOf('.');
+    const indexMiddle = domains[i].lastIndexOf('.', indexLast - 1);
+    const dns1 = domains[i].slice(indexLast + 1);
+    const dns2 = domains[i].slice(indexMiddle + 1, indexLast);
+    const dnsName = `.${dns1}.${dns2}`;
+    let result = 0;
+    for (let j = 0; j < domains.length; j++) {
+      if (
+        domains[j].endsWith(`.${dns2}.${dns1}`) || domains[j] === `${dns2}.${dns1}`
+      ) {
+        result++;
+      }
+    }
+    obj[dnsName] = result;
+  }
+  for (let i = 0; i < domains.length; i++) {
+    let nextDot;
+    const dot = domains[i].lastIndexOf('.');
+    if (
+      domains[i].lastIndexOf('.', dot - 1) >= 0 || domains[i].lastIndexOf('.', dot - 1) !== -1
+    ) {
+      nextDot = domains[i].lastIndexOf('.', dot - 1);
+    }
+    if (domains[i].lastIndexOf('.', nextDot - 1) === -1) {
+      const dns1 = domains[i].slice(dot + 1);
+      const dns2 = domains[i].slice(nextDot + 1, dot);
+      const dns3 = domains[i].slice(0, nextDot);
+      const dnsName = `.${dns1}.${dns2}.${dns3}`;
+      obj[dnsName] = 1;
+    }
+  }
+  return obj;
 }
 
 module.exports = getDNSStats;
